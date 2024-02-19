eee0c9c2992a78e8ba03e2953edb49ac305e9dea
Marek-Motyl <motylmarek90@gmail.com>
2024-02-19 16:04:25 +0100

    feat: 404 error handling

---
+++ apps/frontend/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+        <div className="flex flex-col items-center gap-10 mt-20">
+            <h1 className="text-6xl">404</h1>
+            <div className="text-l md:text-xl">Sorry, this page does not exist</div>
+        </div>
+    );
+}
